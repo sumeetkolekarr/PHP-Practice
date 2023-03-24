952439323adfcbeb7faf1296718b9ce099371a90
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-03-24 14:24:55 +0530

    Changes to be committed: 	new file:   Code With Harry/Basics/Index.php 	new file:   Code With Harry/HTML Files/Index.php 	new file:   Code With Harry/Variables/Index.php

---
+++ Code With Harry/Basics/Index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hello World 1";
+?>
+
+<?php
+echo "Hello World 2";
+?>
+
+<?php
+echo "Hello World 3";
+?>
+
+<?php
+echo 500+630
+?>
---
+++ Code With Harry/HTML Files/Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "This is "; echo "Title";?></title>
+</head>
+<body>
+   <p>
+   <?php
+//    This is Hello World
+/* 
+This 
+is
+my
+Multiline
+Comments
+*/
+    echo "Hello World 1";
+    ?>
+    </p> 
+</body>
+</html>
