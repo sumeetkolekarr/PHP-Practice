159c06c5ff7692932b5574294f4feda682ece53b
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-03-25 01:13:04 +0530

    Changes to be committed: 	new file:   Code With Harry/Quick Quiz/Driver/Index.php 	deleted:    Quick Quiz/Driver/Index.php

---
+++ Code With Harry/Quick Quiz/Driver/Index.php
@@ -0,0 +1,17 @@
+<?php
+$age = 65;
+
+if (($age>25) && ($age<=65)){
+    echo "You can Drive";
+}
+elseif (($age>0) && ($age<25)) {
+    echo "You can not Drive";
+}
+elseif (($age>65)) {
+    echo "You can not Drive";
+}
+else {
+    echo "You can not Drive";
+}
+
+?>
