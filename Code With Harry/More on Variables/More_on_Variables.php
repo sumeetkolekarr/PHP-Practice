d13f4bdcbec2a92560b15cb98089e33028548a9e
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-03-24 14:59:51 +0530

    Changes to be committed: 	new file:   Code With Harry/More on Variables/More_on_Variables.php

---
+++ Code With Harry/More on Variables/More_on_Variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>More on Variables</title>
+</head>
+<body>
+    <?php
+    echo "More on Variables <br>";
+    ?>
+    <h3>Rules for creating variables in PHP</h3>
+    <ul>
+        <li>Starts with a $ sign</li>
+        <li>Cannot start with a number</li>
+        <li>Must start with a letter or an underscore character</li>
+        <li>Can only contain alphanumeric characters and underscore</li>
+        <li>Variables in PHP are Case Sensitive</li>
+    </ul>
+</body>
+</html>
